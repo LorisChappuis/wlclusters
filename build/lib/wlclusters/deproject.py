100dfe1ff5ecc8d7b1ab0f3fd6985b9245cd748c
Loris Chappuis <lorischappuis@hotmail.fr>
2024-09-23 18:11:39 +0200

    fighting with git

--- build/lib/wlclusters/deproject.py
+++ build/lib/wlclusters/deproject.py
@@ -2,6 +2,7 @@ import numpy as np
 from astropy.cosmology import Planck15 as cosmo
 import astropy.units as u
 
+
 class MyDeprojVol:
     """
     A class to handle the deprojection of volumes for spherical shells.
@@ -13,6 +14,7 @@ class MyDeprojVol:
     Methods:
         deproj_vol(): Computes the deprojected volume matrix for the given radii.
     """
+
     def __init__(self, radin, radot):
         """
         Initialize the MyDeprojVol class with inner and outer radii.
@@ -50,27 +52,41 @@ class MyDeprojVol:
 
         diftot = 0
         for i in range(1, len(ri)):
-            dif = abs(ri[i] - ro[i-1]) / ro[i-1] * 100.
+            dif = abs(ri[i] - ro[i - 1]) / ro[i - 1] * 100.0
             diftot += dif
-            ro[i-1] = ri[i]
+            ro[i - 1] = ri[i]
 
         if abs(diftot) > 0.1:
-            print(' DEPROJ_VOL: WARNING - abs(ri(i)-ro(i-1)) differs by', diftot, ' percent')
-            print(' DEPROJ_VOL: Fixing up radii ... ')
-            for i in range(1, len(ri)-1):
-                dif = abs(ri[i] - ro[i-1]) / ro[i-1] * 100.
+            print(
+                " DEPROJ_VOL: WARNING - abs(ri(i)-ro(i-1)) differs by",
+                diftot,
+                " percent",
+            )
+            print(" DEPROJ_VOL: Fixing up radii ... ")
+            for i in range(1, len(ri) - 1):
+                dif = abs(ri[i] - ro[i - 1]) / ro[i - 1] * 100.0
                 diftot += dif
 
         nbin = len(ro)
-        volconst = 4. / 3. * np.pi
+        volconst = 4.0 / 3.0 * np.pi
         volmat = np.zeros((nbin, nbin))
 
         for iring in list(reversed(range(0, nbin))):
-            volmat[iring, iring] = volconst * ro[iring]**3 * (1. - (ri[iring] / ro[iring])**2.)**1.5
-            for ishell in list(reversed(range(iring+1, nbin))):
-                f1 = (1. - (ri[iring] / ro[ishell])**2.)**1.5 - (1. - (ro[iring] / ro[ishell])**2.)**1.5
-                f2 = (1. - (ri[iring] / ri[ishell])**2.)**1.5 - (1. - (ro[iring] / ri[ishell])**2.)**1.5
-                volmat[ishell, iring] = volconst * (f1 * ro[ishell]**3 - f2 * ri[ishell]**3)
+            volmat[iring, iring] = (
+                volconst
+                * ro[iring] ** 3
+                * (1.0 - (ri[iring] / ro[iring]) ** 2.0) ** 1.5
+            )
+            for ishell in list(reversed(range(iring + 1, nbin))):
+                f1 = (1.0 - (ri[iring] / ro[ishell]) ** 2.0) ** 1.5 - (
+                    1.0 - (ro[iring] / ro[ishell]) ** 2.0
+                ) ** 1.5
+                f2 = (1.0 - (ri[iring] / ri[ishell]) ** 2.0) ** 1.5 - (
+                    1.0 - (ro[iring] / ri[ishell]) ** 2.0
+                ) ** 1.5
+                volmat[ishell, iring] = volconst * (
+                    f1 * ro[ishell] ** 3 - f2 * ri[ishell] ** 3
+                )
 
                 if volmat[ishell, iring] < 0.0:
                     exit()
